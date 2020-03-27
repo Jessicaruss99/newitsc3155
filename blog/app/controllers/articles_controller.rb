4bee85e271e219a0a321ab52e120c22d4acde8cb
Jessica Russ <jruss8@uncc.edu>
2020-03-27 03:11:47 +0000

    List all articles

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end 
+    
     def new
     end
     
     def create
-        @article = Article.new(params[:article])
+        @article = Article.new(article_params)
         
         @article.save
         redirect_to @article
@@ -12,9 +16,10 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end 
+    
+    private 
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end 
 end
 
-private 
-def article_params
-    params.require(:article).permit(:title, :text)
-end 
