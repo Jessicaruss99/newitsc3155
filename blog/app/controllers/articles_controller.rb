edeff420448740a38d3ba37b4fa2ae6df945ca91
Jessica Russ <jruss8@uncc.edu>
2020-03-27 03:26:05 +0000

    add error messages for title validations

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -4,15 +4,22 @@ class ArticlesController < ApplicationController
     end 
     
     def new
+        @article = Article.new
     end
     
     def create
         @article = Article.new(article_params)
         
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end 
+        # @article.save
+        # redirect_to @article
         # render plain: params[:article].inspect
     end 
+    
     def show
         @article = Article.find(params[:id])
     end 
