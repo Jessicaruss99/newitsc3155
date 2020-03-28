f9208b051f2741b7b0789711d1e482515d991c82
Jessica Russ <jruss8@uncc.edu>
2020-03-28 00:22:25 +0000

    Add edit/update functionality

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -20,6 +20,19 @@ class ArticlesController < ApplicationController
         # render plain: params[:article].inspect
     end 
     
+    def edit
+        @article = Article.find(params[:id])
+    end 
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end 
+    end 
+    
     def show
         @article = Article.find(params[:id])
     end 
